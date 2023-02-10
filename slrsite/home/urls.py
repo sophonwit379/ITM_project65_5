157c0bc9d3bc3d494a83e4444c5adec694464d54
CptSnowball-G <78529993+sophonwit379@users.noreply.github.com>
2023-02-10 11:40:42 +0700

    linked complete

--- slrsite/home/urls.py
+++ slrsite/home/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.Index)
+    path('',views.Index,name='index'),
+    path('about',views.About,name='about'),
+    path('team',views.Team,name='team'),
+    path('ai',views.Ai,name='ai')
 ]
--- slrsite/home/views.py
+++ slrsite/home/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def Index(request):
     return render(request,'index.html')
+
+def About(request):
+    return render(request,'about.html')
+
+def Team(request):
+    return render(request,'team.html')
+
+def Ai(request):
+    return render(request,'ai.html')
